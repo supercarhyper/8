f83c95510449810cca6288e33d26716e696a7d58
supercarhyper <88059326+supercarhyper@users.noreply.github.com>
2021-08-14 11:55:55 -0500

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,9 @@
+function test50(x, y) 
+{
+  return ((x == 50 || y == 50) || (x + y == 50));
+}
+document.write(test50(50, 50))
+console.log(test50(20, 50))
+console.log(test50(20, 20))
+console.log(test50(20, 30))
+
